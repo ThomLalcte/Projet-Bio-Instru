8ba1228a8270e9449160395e630e6edb3c14c563
MrMushroomkiller <mr.mushroomkiller@gmail.com>
2022-11-29 14:31:11 -0500

    rms algo

--- plotcsv.py
+++ plotcsv.py
@@ -1,12 +1,22 @@
 from matplotlib import pyplot as plt
 import csv
+import numpy as np
 
-with open("first flex.csv","r") as file:
+with open("force faible.csv","r") as file:
     csvreader = csv.reader(file)
-    data:list[int] = []
+    data = []
     for row in csvreader:
+        roww = []
         for sample in row:
+            # roww.append(int(sample))
             data.append(int(sample))
+        # data.append(roww)
     file.close()
+    data = np.array(data)
+    tempdata = np.square(data-data.mean())
+    rms = []
+    for offset in range(10,len(tempdata)-10):
+        rms.append(np.sqrt(np.mean(tempdata[offset-10:offset+10])))
     plt.plot(data)
+    plt.plot(rms)
     plt.show()
